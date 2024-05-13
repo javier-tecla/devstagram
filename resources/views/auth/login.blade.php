82f229c7ded14deccc77be25cf49c750eebfbed0
javierBorjas <cristman11@gmail.com>
2024-05-13 00:04:16 -0300

    Mantener la sesión siempre abierta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,6 +55,10 @@
                                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                             @enderror
                         </div>
+
+                        <div class="mb-5">
+                            <input type="checkbox" name="remember"> <label class="text-gray-500 text-sm">Mantener mi sesión abierta</label>
+                        </div>
                        
                                 <input 
                                     type="submit"
