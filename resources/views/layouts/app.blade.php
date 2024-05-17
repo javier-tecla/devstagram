2ddbcdecf7e74fbb7e38bf2ceca3800e6bf9daaf
javierBorjas <cristman11@gmail.com>
2024-05-16 21:50:21 -0300

    Instalando Dropzone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                               
                         Crear
                     </a>
-                    <a class="font-bold text-gray-600 text-sm" href="#">
+                    <a class="font-bold text-gray-600 text-sm" href="{{ route('posts.index', auth()->user()->username ) }}">
                         Hola: <span class="font-normal"> {{ auth()->user()->username }}</span>
                     </a>
 
