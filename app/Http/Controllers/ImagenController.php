2f050c16defbbac0ceb47d949e93a068249b9558
javierBorjas <cristman11@gmail.com>
2024-05-16 22:30:54 -0300

    Leer las imágenes del Request

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class ImagenController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        return "Desde Imagen Controller";
+        $imagen = $request->file('file');
+
+        return response()->json(['imagen' => $imagen->extension() ]);
     }
 }
