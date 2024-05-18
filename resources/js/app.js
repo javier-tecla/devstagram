9b40e059b357a58bf9c8fa66492fb299afc4c351
javierBorjas <cristman11@gmail.com>
2024-05-18 15:28:02 -0300

    Agregar la imagen a Dropzone si la validación falla pero tenemos 1 imagen

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,30 @@ const dropzone = new Dropzone("#dropzone", {
     dictDefaultMessage: "Sube aquí tu imagen",
     acceptedFiles: ".png,.jpg, .jpeg,.gif",
     addRemoveLinks: true,
-    dictRemoveFile: 'Borrar Archivo',
+    dictRemoveFile: "Borrar Archivo",
     maxFiles: 1,
     uploadMultiple: false,
+
+    init: function () {
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+            imagenPublicada.name =
+                document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(
+                this,
+                imagenPublicada,
+                `/uploads/${imagenPublicada.name}`
+            );
+
+            imagenPublicada.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    },
 });
 
 dropzone.on("success", function (file, response) {
@@ -16,4 +37,5 @@ dropzone.on("success", function (file, response) {
 });
 
 dropzone.on("removedfile", function () {
+    document.querySelector('[name="imagen"]').value = "";
 });
