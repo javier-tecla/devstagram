dc4b1c70b2fc59dbfb50439ef95a9b18145a4a30
javierBorjas <cristman11@gmail.com>
2024-05-19 18:05:07 -0300

    Almacenando el Post con una relación

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,12 +46,19 @@ class PostController extends Controller implements HasMiddleware
 
 
         // Otra forma
-        $post = new Post;
-        $post->titulo = $request->titulo;
-        $post->descripcion = $request->descripcion;
-        $post->imagen = $request->imagen;
-        $post->user_id = auth()->user()->id;
-        $post->save();
+        // $post = new Post;
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request->imagen;
+        // $post->user_id = auth()->user()->id;
+        // $post->save();
+
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
