5659629d034eda3d4a65dc96671fb531d82f368e
javierBorjas <cristman11@gmail.com>
2024-05-20 02:18:28 -0300

    Almacenando los Comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comentario;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
-        dd('Comentando...');
+        // validar
+        $request->validate([
+            'comentario' => 'required|max:255'
+        ]);
+
+
+        // almacenar e resultado
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
+
+
+        // Imprimir un mensaje
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
     }
 }
