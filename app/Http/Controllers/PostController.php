db5083a242c609b7e8a8a52859ada174e86dc781
javierBorjas <cristman11@gmail.com>
2024-05-19 23:11:10 -0300

    Mejorando la estructura de las URL's

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -66,7 +66,7 @@ class PostController extends Controller implements HasMiddleware
         return redirect()->route('posts.index', auth()->user()->username);
     }
 
-    public function show(Post $post)
+    public function show(User $user, Post $post)
     {
         return view('posts.show', [
             'post' => $post
