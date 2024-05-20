8263d7ca99fe6dda9e98014c00f72c10ee4b8ba7
javierBorjas <cristman11@gmail.com>
2024-05-19 21:35:25 -0300

    Mostrar mensaje cuando no hay posts de forma condicional

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,6 +36,10 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
+        @if($posts->count())
+            
+        
+
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             @foreach ($posts as $post)
                 <div>
@@ -46,6 +50,11 @@
                 
             @endforeach
         </div>
+
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+
+        @endif
     </section>
 
 @endsection
