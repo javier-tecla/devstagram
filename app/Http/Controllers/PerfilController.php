00b0448cd317ba6c975341b34cc8cc3ee06b35f6
javierBorjas <cristman11@gmail.com>
2024-05-21 22:45:09 -0300

    Almacenar la información del Usuario y Crear la migración para añadir imagenes al perfil del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Routing\Controllers\HasMiddleware;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class PerfilController extends Controller implements HasMiddleware
 { 
@@ -35,5 +38,35 @@ class PerfilController extends Controller implements HasMiddleware
         $this->validate($request, [
             'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
         ]);
+
+        if($request->imagen) {
+            $manager = new ImageManager(new Driver());
+ 
+            $imagen = $request->file('imagen');
+     
+            //generar un id unico para las imagenes
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+     
+            //guardar la imagen al servidor
+            $imagenServidor = $manager->read($imagen);
+            //agregamos efecto a la imagen con intervention
+            $imagenServidor->cover(1000, 1000);
+            // la unidad de mide en PX 1= 1pixiel
+     
+            //agregamos la imagen a la  carpeta en public donde se guardaran las imagenes
+            $imagenesPath = public_path('perfiles') . '/' . $nombreImagen;
+            //Una vez procesada la imagen entonces guardamos la imagen en la carpeta que creamos
+            $imagenServidor->save($imagenesPath);
+        } 
+
+        // Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null;
+        $usuario->save();
+
+        // Redireccionar
+        return redirect()->route('posts.index', $usuario->username);
+
     }
 }
